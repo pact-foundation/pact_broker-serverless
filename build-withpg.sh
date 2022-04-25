1171ad640af6fc6a8588969dc6f3a0db4b29f31d
Yousaf Nabi <yousafn@gmail.com>
2022-04-25 21:33:27 +0100

    fix: update to ruby2.7 runtime

--- build-withpg.sh
+++ build-withpg.sh
@@ -5,9 +5,9 @@ set -e
 rm -rf lib && mkdir -p lib
 rm -rf vendor
 
-docker build -t ruby25-pg-builder -f Dockerfile .
+docker build -t ruby27-pg-builder -f Dockerfile .
 
-CONTAINER=$(docker run -d ruby25-pg-builder false)
+CONTAINER=$(docker run -d ruby27-pg-builder false)
 
 docker cp \
     $CONTAINER:/usr/lib64/libpq.so.5.5 \
