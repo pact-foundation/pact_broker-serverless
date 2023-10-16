0a9030c50e3511e2209a7ff405c86d6bc473f6aa
Yousaf Nabi <yousaf@pactflow.io>
2023-10-16 16:19:16 +0100

    chore: update deps to ruby3.2 and psql14

--- build-withpglayer.sh
+++ build-withpglayer.sh
@@ -4,9 +4,9 @@ set -e
 
 rm -rf vendor
 
-docker build -t ruby27-pg-builder -f Dockerfile .
+docker build -t ruby32-pg-builder -f Dockerfile .
 
-CONTAINER=$(docker run -d ruby27-pg-builder false)
+CONTAINER=$(docker run -d ruby32-pg-builder false)
 
 docker cp \
     $CONTAINER:/var/task/vendor \
--- rubypg_layer/build.sh
+++ rubypg_layer/build.sh
@@ -4,13 +4,44 @@ set -e
 
 rm -rf layer && mkdir -p layer/lib
 
-docker build -t ruby25-pg-builder -f Dockerfile .
+docker build -t ruby32-pg-builder -f Dockerfile .
 
-CONTAINER=$(docker run -d ruby25-pg-builder false)
+CONTAINER=$(docker run -d ruby32-pg-builder false)
 
 docker cp \
-    $CONTAINER:/usr/lib64/libpq.so.5.5 \
-    layer/lib/libpq.so.5
+  $CONTAINER:/usr/lib64/libpq.so.5.14 \
+  layer/lib/libpq.so.5
 
+docker cp \
+  $CONTAINER:/usr/lib64/libldap_r-2.4.so.2.10.7 \
+  layer/lib/libldap_r-2.4.so.2
+
+docker cp \
+  $CONTAINER:/usr/lib64/liblber-2.4.so.2.10.7 \
+  layer/lib/liblber-2.4.so.2
+
+docker cp \
+  $CONTAINER:/usr/lib64/libsasl2.so.3.0.0 \
+  layer/lib/libsasl2.so.3
+
+docker cp \
+  $CONTAINER:/usr/lib64/libssl3.so \
+  layer/lib/
+
+docker cp \
+  $CONTAINER:/usr/lib64/libsmime3.so \
+  layer/lib/
+
+docker cp \
+  $CONTAINER:/usr/lib64/libnss3.so \
+  layer/lib/
+
+docker cp \
+  $CONTAINER:/usr/lib64/libnssutil3.so \
+  lib/
+
+docker cp \
+  $CONTAINER:/var/task/vendor \
+  vendor
 docker stop $CONTAINER
 docker rm $CONTAINER
